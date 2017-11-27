def39f9f2030955bd56994403d6dec6ff69ef77a
KengoRag <kengoraganit@gmail.com>
2017-11-27 11:06:50 -1000

    nihongo

--- assignment.js
+++ assignment.js
@@ -1,23 +1,20 @@
 
-var output;
+
 var greenAliens = 6000;
 var blueAliens = 1000;
 var redAliens = 3000;
 
 function alienChecker(x, y, z) {
 	if (( x + y + z == 10000) && x > 5000) {
-		return  "MOTHER OF GOD, not the green ones!";
+		console.log("MOTHER OF GOD, not the green ones!");
 
 	} if (( x + y + z == 10000) && x + z > 8000) {
-		return  "DOOOOOOOOM";
+		console.log("DOOOOOOOOM");
 
 	} if (( x + y + z == 10000) && y + z < 3000) {
-		return  "We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY!";
+		console.log("We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY!");
 	}
 
 }
 
 alienChecker(greenAliens, blueAliens, redAliens);
-
-console.log("MOTHER OF GOD, not the green ones!" + "DOOOOOOOOM")
-
