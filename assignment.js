f29e6ed85588b036c74e6c16c4c73d758b545fba
unknown <19kennit@waipahuh.k12.hi.us>
2017-11-26 22:09:46 -1000

    日本語

--- assignment.js
+++ assignment.js
@@ -5,19 +5,19 @@ var blueAliens = 1000;
 var redAliens = 3000;
 
 function alienChecker(x, y, z) {
-	if ( (x + y + z) = 10000 && x > 5000) {
-		output =  "MOTHER OF GOD, not the green ones!";
+	if (( x + y + z == 10000) && x > 5000) {
+		return  "MOTHER OF GOD, not the green ones!";
 
-	} if ( (x + y + z) = 10000 && x + z > 8000) {
-		output =  "DOOOOOOOOM";
+	} if (( x + y + z == 10000) && x + z > 8000) {
+		return  "DOOOOOOOOM";
 
-	} if ( (x + y + z) = 10000 && y + z < 3000) {
-		output =  "We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY!";
+	} if (( x + y + z == 10000) && y + z < 3000) {
+		return  "We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY!";
 	}
 
 }
 
 alienChecker(greenAliens, blueAliens, redAliens);
 
-console.log("MOTHER OF GOD, not the green ones!" + " DOOOOOOOOM");
+console.log("MOTHER OF GOD, not the green ones!" + "DOOOOOOOOM")
 
